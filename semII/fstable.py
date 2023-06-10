01cfe60210386cc3945550cee855e96ef3b73f3f
Aditya Godse <adimail2404@gmail.com>
2023-06-10 17:44:01 +0530

    Update fstable.py

--- semII/fstable.py
+++ semII/fstable.py
@@ -7,7 +7,7 @@ a=math.sin(r2d(90))
 b=math.cos(r2d(84))
 
 x=[0,30,60,90,120,150]
-y=[0,902,19.6,17.8,17.3,11.7]
+y=[0,09.2,14.6,17.8,17.3,11.7]
 
 # tblrow=[6][5]
 
@@ -30,17 +30,23 @@ y=[0,902,19.6,17.8,17.3,11.7]
 
 
 tblrow=[]
-rows, cols=5,5
+rows, cols=6,6
 
 tblrow = [ [0 for i in range(cols)] for j in range(rows)]
 
-# for i in range(len(tblrow)):
-#     for j in range(len(tblrow[i])):
-#         tblrow[]
+for i in range(rows):
+    tblrow[i][0]=x[i]
+    tblrow[i][1]=y[i]
 
-tblrow[1][3]=5
+for r in range(len(tblrow)):
+    for c in range(len(tblrow[r])):
+        tblrow[r][2] = y[r]*math.sin(r2d(x[r]))
+        tblrow[r][3] = y[r]*math.sin(r2d(2*x[r]))
+        tblrow[r][4] = y[r]*math.cos(r2d(x[r]))
+        tblrow[r][5] = y[r]*math.cos(r2d(2*x[r]))
+
 
 for i in tblrow:
     for j in i:
-        print(j,end="\t")
-    print()
+        print(round(j,2),end="\t\t")
+    print("\n")
