7e71be6d98f591528fd3f75dd2ee8568a3cbfb35
Aditya Godse <adimail2404@gmail.com>
2023-06-10 17:22:40 +0530

    Update fstable.py

--- semII/fstable.py
+++ semII/fstable.py
@@ -9,20 +9,36 @@ b=math.cos(r2d(84))
 x=[0,30,60,90,120,150]
 y=[0,902,19.6,17.8,17.3,11.7]
 
-tblrow=[6][5]
+# tblrow=[6][5]
 
-for i in range(0,6):
-    tblrow[i][0] = y[i]*math.sin(r2d(x[i]))
-    tblrow[i][1] = y[i]*math.sin(r2d(2*x[i]))
-    tblrow[i][2] = y[i]*math.cos(r2d(x[i]))
-    tblrow[i][3] = y[i]*math.cos(r2d(2*x[i])) 
+# for i in range(0,6):
+#     tblrow[i][0] = y[i]*math.sin(r2d(x[i]))
+#     tblrow[i][1] = y[i]*math.sin(r2d(2*x[i]))
+#     tblrow[i][2] = y[i]*math.cos(r2d(x[i]))
+#     tblrow[i][3] = y[i]*math.cos(r2d(2*x[i])) 
 
-rows, cols = (6, 6)
-tbl = [[0]*cols]*rows
+# rows, cols = (6, 6)
+# tbl = [[0]*cols]*rows
 
-print()
+# print()
 
-for i in tbl:
+# for i in tbl:
+#     for j in i:
+#         print(j,end="\t")
+#     print()
+
+
+
+tblrow=[]
+rows, cols=5,5
+for i in range(rows):
+    col = []
+    for j in range(cols):
+        col.append(0)
+    tblrow.append(col)
+
+
+for i in tblrow:
     for j in i:
         print(j,end="\t")
     print()
